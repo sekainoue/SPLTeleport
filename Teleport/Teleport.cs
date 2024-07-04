feacdf4bb79c0053298c66161d84bc9d1f160c19
sekainoue <sekainouexr@gmail.com>
2024-07-04 22:27:36 +0900

    lockPosition and input floats

--- Teleport/Teleport.cs
+++ Teleport/Teleport.cs
@@ -3,6 +3,9 @@ using SharpPluginLoader.Core.Entities;
 using SharpPluginLoader.Core;
 using SharpPluginLoader.Core.IO;
 using System.Numerics;
+using System.Threading;
+using SharpPluginLoader.Core.Actions;
+using System;
 
 namespace Teleport
 {
@@ -18,24 +21,38 @@ namespace Teleport
         public void OnQuestReturn(int questId) { _selectedMonster = null; }
         public void OnQuestAbandon(int questId) { _selectedMonster = null; }
 
-        private Vector3 _playerPosition = new Vector3(0f, 0f, 0f);
-        public float _movementAmount = 10.0f;
+        private Vector3 _currentPosition;
+        private Vector3 _lastPosition;
+        public float _movementAmount = 1f;
+        private float _minMovementAmount = -1000.0f;
+        private float _maxMovementAmount = 1000.0f;
         private bool _lockPosition = false;
-       
+        private Vector3 _inputPosition = new Vector3(0f, 0f, 0f);
+        private float _minInputPos = -5000000.000f;
+        private float _maxInputPos = 5000000.000f;
+
+        public static float Clamp(float value, float min, float max)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
         public unsafe void OnImGuiRender()
         {
             var player = Player.MainPlayer;
             if (player is null)
                 return;
-
-            if (ImGui.Button("noclip Toggle")) {
+            
+            if (ImGui.Button("Toggle noclip")) {
                 if (!_lockPosition)
-                { _lockPosition = true; } 
+                { _lockPosition = true; player.PauseAnimations(); } 
                 else 
-                { _lockPosition = false; }
+                { _lockPosition = false; player.ResumeAnimations(); }
             }
+            ImGui.InputFloat("Velocity", ref _movementAmount, 0.0f, 300.0f);
+            _movementAmount = Clamp(_movementAmount, _minMovementAmount, _maxMovementAmount);
 
-            if (ImGui.BeginCombo("Teleport to Monster:", (_selectedMonster is not null ? $"{_selectedMonster.Type} @ 0x{_selectedMonster.Instance:X}" : "None"))) // Instance shows address of pointer
+            if (ImGui.BeginCombo("", (_selectedMonster is not null ? $"{_selectedMonster.Type} @ 0x{_selectedMonster.Instance:X}" : "None"))) // Instance shows address of pointer
             {
                 var monsters = Monster.GetAllMonsters().TakeLast(5).ToArray();
                 foreach (var monster in monsters)
@@ -47,27 +64,78 @@ namespace Teleport
                 }
                 ImGui.EndCombo();
             }
-            if (ImGui.Button("Go"))
+            if (ImGui.Button("Monster Here"))
             {
                 if (_selectedMonster == null)
                     return;
-                player.Teleport(_selectedMonster.Position);
+                _selectedMonster.Teleport(player.Position);
+            }
+
+            ImGui.InputFloat3("", ref _inputPosition);
+            _inputPosition.X = Clamp(_inputPosition.X, _minInputPos, _maxInputPos);
+            _inputPosition.Y = Clamp(_inputPosition.Y, _minInputPos, _maxInputPos);
+            _inputPosition.Z = Clamp(_inputPosition.Z, _minInputPos, _maxInputPos);
+
+            if (ImGui.Button("Player To"))
+            {
+                if (!_lockPosition)
+                { _lockPosition = true; player.PauseAnimations(); }
+                else
+                { _lockPosition = false; player.ResumeAnimations(); }
+                player.Teleport(_inputPosition);
             }
         }
-
-        public void OnUpdate (float deltatime)
+        public void OnUpdate(float deltaTime)
         {
             var player = Player.MainPlayer;
             if (player is null)
                 return;
-            _playerPosition = player.Position;
-            if (_lockPosition) {
-                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.UpArrow)) { _playerPosition.Z -= _movementAmount; player.Teleport(_playerPosition); }
-                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.DownArrow)) { _playerPosition.Z += _movementAmount; player.Teleport(_playerPosition); }
-                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.PageUp)) { _playerPosition.Y += _movementAmount; player.Teleport(_playerPosition); }
-                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.PageDown)) { _playerPosition.Y -= _movementAmount; player.Teleport(_playerPosition); }
-                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.LeftArrow)) { _playerPosition.X -= _movementAmount; player.Teleport(_playerPosition); }
-                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.RightArrow)) { _playerPosition.X += _movementAmount; player.Teleport(_playerPosition); }
+            _currentPosition = player.Position;
+
+            if (_lockPosition)
+            {
+                player.Teleport(_lastPosition);
+
+                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.UpArrow))
+                {
+                    _currentPosition.Z -= _movementAmount;
+                    player.Teleport(_currentPosition);
+                   _lastPosition = _currentPosition;
+                }
+                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.DownArrow))
+                {
+                    _currentPosition.Z += _movementAmount;
+                    player.Teleport(_currentPosition);
+                    _lastPosition = _currentPosition; 
+                }
+                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.PageUp))
+                {
+                    _currentPosition.Y += _movementAmount;
+                    player.Teleport(_currentPosition); 
+                    _lastPosition = _currentPosition; 
+                }
+                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.PageDown))
+                {
+                    _currentPosition.Y -= _movementAmount;
+                    player.Teleport(_currentPosition); 
+                    _lastPosition = _currentPosition; 
+                }
+                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.LeftArrow))
+                {
+                    _currentPosition.X -= _movementAmount;
+                    player.Teleport(_currentPosition); 
+                    _lastPosition = _currentPosition; 
+                }
+                if (Input.IsDown(Key.LeftAlt) && Input.IsDown(Key.RightArrow))
+                {
+                    _currentPosition.X += _movementAmount;
+                    player.Teleport(_currentPosition); 
+                    _lastPosition = _currentPosition; 
+                }
+            }
+            else
+            {
+                _lastPosition = player.Position;
             }
         }
     }
